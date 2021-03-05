0a502ac086686346cf2dad0d8bd51a2987e7a75c
gatiskevans <gatis.kevans@gmail.com>
2021-03-06 01:32:58 +0200

    .

---
+++ src/main/Divi.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Divi {
+    //Var arī izveidot objektu klasē un piekļūt pie tā no citas klases.
+    //To parasti izmanto, lai labāk organizētu klases
+    //Vienā klasē ir visi atribūti un metodes, kamēr otrā ir main() metode, kura izpilda kodu
+    int a = 20;
+    final int b = 48;       //Ja mēģinās pārraktīt šo vērtību, tad leks erro paziņojums, jo ir uzstādīts kā final
+
+    String vards = "Jānis";
+    String uzvards = "Bērziņš";
+
+}
--- src/main/Otrais.java
+++ src/main/Otrais.java
@@ -1,9 +1,44 @@
 package main;
 
+//klase vienmēr sākas ar lielo burtu un klases nosaukumam ir jābūt vienādam ar java failu
 public class Otrais {
+
+    int x = 5;
+
     public static void main(String[] args) {
 
-        System.out.println("Hello");
+        //Klases nosaukums pēc tam objekta nosaukums vienāds ar new komandu un klases nosaukums
+        Otrais mansObjekts = new Otrais();
 
+        //Izdrukās laikā objektu, kur int x paķerts no klases, jo objekti manto klases mainīgos
+        System.out.println(mansObjekts.x);
+
+        //Var izveidot arī vairākus objektus vienā klasē
+        Otrais mansObjekts2 = new Otrais();
+        System.out.println(mansObjekts2.x);
+
+        //Piekļūstam klāt datiem no citas klases, citā failā
+        Divi objekts3 = new Divi();
+        System.out.println(objekts3.a);
+
+        //Mainīgie klasēs patiesībā saucas atribūti. Šajā gadījumā x būs atribūts.
+        //Vēlviens termins priekš klašu atribūtiem ir fields
+        //Piekļūt atribūtiem var, izveidojot jaunu klases objektu un tad ar . simbolu atlasīt, kuru atribūtu izmantos
+
+        //Var arī modificēt, definēt un mainīt atribūtu vērtības
+        objekts3.a = 50;
+        System.out.println(objekts3.a);
+        System.out.println(objekts3.b);
+
+        //Ja divi objekti piekļūst tiem pašiem atribūtiem vienā klasē, tos var izmainīt,
+        //bet viena objekta izmaiņas neietekmēs otra objekta atribūta vērtību
+        Divi objekts4 = new Divi();
+        //Atcerieties, ka atribūts a tika nomainīts uz 50, taču objekts4 to attēlos kā 20
+        System.out.println(objekts4.a);
+
+        //Var izmantot cik daudz atribūtus vēlas
+        Divi objekts5 = new Divi();
+        System.out.println("Mans vārds ir " + objekts5.vards + " " + objekts5.uzvards + " un man ir " + objekts5.a + " gadu.");
+        
     }
 }
